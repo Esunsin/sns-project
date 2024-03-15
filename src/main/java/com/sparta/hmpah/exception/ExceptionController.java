3753561102d557ba0e857b7100eb7e39011e135e
Esunsin <jinchan1148@gmail.com>
2024-03-15 15:14:44 +0900

    feat : customException

--- src/main/java/com/sparta/hmpah/exception/ExceptionController.java
+++ src/main/java/com/sparta/hmpah/exception/ExceptionController.java
@@ -1,10 +1,17 @@
 package com.sparta.hmpah.exception;
 
+import com.sparta.hmpah.dto.ExceptionDto;
 import com.sparta.hmpah.dto.responseDto.ExceptionResponse;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class ExceptionController {
@@ -12,5 +19,38 @@ public class ExceptionController {
     @ExceptionHandler({IllegalArgumentException.class})
     public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(final IllegalArgumentException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionResponse(400, ex.getMessage()));
+    }
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<ExceptionDto> handleDuplicateKeyException(DuplicateKeyException e) {
+        return createResponse(HttpStatus.CONFLICT, e.getMessage());
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ExceptionDto> handleNoSuchElementException(NoSuchElementException e) {
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ExceptionDto> handleBadCredentialsException(BadCredentialsException e) {
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ExceptionDto> handleAuthenticationException(AuthenticationException e) {
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ExceptionDto> handleAccessDeniedException(AccessDeniedException e) {
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
+
+    private ResponseEntity<ExceptionDto> createResponse(HttpStatus status, String message) {
+        return ResponseEntity.status(status.value())
+                .body(ExceptionDto.builder()
+                        .statusCode(status.value())
+                        .state(status)
+                        .message(message)
+                        .build());
     }
 }
