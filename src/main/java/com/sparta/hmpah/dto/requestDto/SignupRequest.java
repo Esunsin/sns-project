bf8316eec3b02cb2bc649a439cb594fbe6b0eac2
Esunsin <jinchan1148@gmail.com>
2024-03-15 15:08:31 +0900

    조건 없앰

--- src/main/java/com/sparta/hmpah/dto/requestDto/SignupRequest.java
+++ src/main/java/com/sparta/hmpah/dto/requestDto/SignupRequest.java
@@ -11,13 +11,10 @@ import org.hibernate.validator.constraints.Email;
 public class SignupRequest extends LoginInfoRequest {
 
     @NotBlank
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d).{4,10}$",
-        message = "아이디는 최소 4자 이상, 10자 이하로 알파벳 소문자와 숫자로 구성되어야 합니다.")
+
     private String username;
 
     @NotBlank
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\W)(?=.*\\d).{8,15}$",
-        message = "비밀번호는 최소 8자 이상, 15자 이하로 알파벳과 특수문자, 숫자로 구성되어야 합니다.")
     private String password;
 
     @Email
