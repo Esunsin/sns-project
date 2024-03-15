4da2d6a0de34acb2d5bc548642e3883de71b34df
Esunsin <jinchan1148@gmail.com>
2024-03-15 15:22:47 +0900

    feat : page 추가

--- src/main/java/com/sparta/hmpah/repository/PostRepository.java
+++ src/main/java/com/sparta/hmpah/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.sparta.hmpah.repository;
 
 import com.sparta.hmpah.entity.Post;
 import com.sparta.hmpah.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +18,10 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query("SELECT p FROM Post p WHERE p.title LIKE CONCAT('%', :keyword, '%')")
     List<Post> findByContainTitleKeyWord(@Param("keyword") String keyWord);
+
+    @Query(value = "select p from Post p"
+            , countQuery = "select count(p) from Post p")
+        // 데이터 양이 많이지면 카운트 커리에 join이 필요 없어서 따로 분리 가능
+    Page<Post> findPageAll(Pageable pageable);
+
 }
