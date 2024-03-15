8085917b330d95b8e51f3363617e315a629a2831
Esunsin <jinchan1148@gmail.com>
2024-03-15 15:05:48 +0900

    jpaqueryfactory 빈 등록

--- src/main/java/com/sparta/hmpah/config/JpaConfig.java
+++ src/main/java/com/sparta/hmpah/config/JpaConfig.java
@@ -1,9 +1,24 @@
 package com.sparta.hmpah.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(entityManager);
+    }
+
 }
