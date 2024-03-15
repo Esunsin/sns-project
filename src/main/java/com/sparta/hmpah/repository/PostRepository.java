7c84f90f063523e289a2fc9018363468ad28e569
Esunsin <jinchan1148@gmail.com>
2024-03-15 15:07:14 +0900

    feat : findByUsername & 제목에 포함된 키워드로 찾기 기능

--- src/main/java/com/sparta/hmpah/repository/PostRepository.java
+++ src/main/java/com/sparta/hmpah/repository/PostRepository.java
@@ -3,9 +3,17 @@ package com.sparta.hmpah.repository;
 import com.sparta.hmpah.entity.Post;
 import com.sparta.hmpah.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByUser(User user);
+
+    @Query("select p from Post p where p.user.username = :username")
+    List<Post> findByUsername(@Param("username") String username);
+
+    @Query("SELECT p FROM Post p WHERE p.title LIKE CONCAT('%', :keyword, '%')")
+    List<Post> findByContainTitleKeyWord(@Param("keyword") String keyWord);
 }
